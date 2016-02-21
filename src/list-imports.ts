e205350a32739fde3d9f7607fe28b68ed9b68c42
David Chandler <davidjchandler@gmail.com>
2016-02-21 18:10:10 +0000

    feat: add import lister.

--- src/list-imports.ts
+++ src/list-imports.ts
@@ -1,15 +1,28 @@
 'use strict';
 
 import vinyl = require('vinyl');
+import * as less from 'less';
 
-function listImports(file: vinyl): Promise<string[]>
-{
-    let promise = new Promise<string[]>((resolve, reject) =>
-    {
-        resolve([]);
+function listImports(file: vinyl): Promise<string[]> {
+    return new Promise<string[]>((resolve, reject) => {
+        if (file == null || file.isNull()) {
+            console.error('Trying to process imports for null file.')
+            return resolve([]);
+        }
+
+        let lessData = file.contents.toString();
+        let options = { filename: file.path };
+
+        less
+            .render(lessData, options)
+            .then(value => {
+                resolve(value.imports);
+            })
+            .catch(reason => {
+                console.error('Failed to process imports for \'' + file.path + '\': ' + reason);
+                resolve([]);
+            });
     });
-    
-    return promise;
 }
 
 export = listImports;
--- test/list-imports.js
+++ test/list-imports.js
@@ -1,17 +1,27 @@
 'use strict';
 
 var chai = require('chai');
-//var es = require('event-stream');
 var File = require('vinyl');
-//var vinylFs = require('vinyl-fs');
-//var FakeFs = require('fake-fs');
 var rewire = require('rewire');
-//var streamAssert = require('stream-assert');
-//var sinon = require('sinon');
+var q = require('q');
+var fs = require('fs');
+var path = require('path');
 
 var listImports = rewire('../release/list-imports');
 
 var expect = chai.expect;
+
+function readFile(file) {
+    return new Promise((resolve, reject) => {
+        q.nfcall(fs.readFile.bind(fs), file.path)
+            .then(data => {
+                file.contents = data;
+                resolve(file);
+            }, reason => {
+                reject(reason);
+            });
+    });
+}
 
 describe('list-imports', () => {
     describe('when passing in a null file', () => {
@@ -31,16 +41,48 @@ describe('list-imports', () => {
     describe('when passing in an empty file', () => {
         let fakeFile = new File({ path: 'something.less', contents: new Buffer('') });
         it('should return an empty list of imports', () => {
-            listImports(fakeFile)
+            return listImports(fakeFile)
+                .then(importList => expect(importList).to.be.empty);
+        });
+    });
+
+    describe('when passing in a file with an import that can\'t be found', () => {
+        let fakeFile = new File({ path: 'something.less', contents: new Buffer('@import "file2.less";') });
+        it('should return the single import', () => {
+            return listImports(fakeFile)
                 .then(importList => expect(importList).to.be.empty);
         });
     });
 
     describe('when passing in a file with an import', () => {
-        let fakeFile = new File({ path: 'something.less', contents: new Buffer('import \'file2.less\';') });
+        const filePath = './test/list-imports-cases/file-with-import/file.less';
         it('should return the single import', () => {
-            return listImports(fakeFile)
-                .then(importList => expect(importList).to.equal([ 'file2.less' ]));
+            return readFile(new File({ path: filePath }))
+                .then(f => listImports(f))
+                .then(importList => expect(importList.map(x => x.split(path.sep).join('!'))).to.deep.equal([
+                    'test!list-imports-cases!file-with-import!import.less']));
+        });
+    });
+
+    describe('when passing in a file with recursive imports', () => {
+        const filePath = './test/list-imports-cases/file-with-recursive-imports/file.less';
+        it('should return the imports', () => {
+            return readFile(new File({ path: filePath }))
+                .then(f => listImports(f))
+                .then(importList => expect(importList.map(x => x.split(path.sep).join('!'))).to.deep.equal([
+                    'test!list-imports-cases!file-with-recursive-imports!import1.less',
+                    'test!list-imports-cases!file-with-recursive-imports!import2.less'
+                ]));
+        });
+    });
+
+    describe('when passing in a file with a data-uri', () => {
+        const filePath = './test/list-imports-cases/file-with-data-uri/file.less';
+        it('should return the referenced image as an import', () => {
+            return readFile(new File({ path: filePath }))
+                .then(f => listImports(f))
+                .then(importList => expect(importList.map(x => x.split(path.sep).join('!'))).to.deep.equal([
+                    'test!list-imports-cases!file-with-data-uri!image.svg']));
         });
     });
 });
