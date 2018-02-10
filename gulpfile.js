150eb8df44a165f200d1efef21e3aad37b4e1061
David Chandler <davidjchandler@gmail.com>
2018-02-10 00:34:07 +0000

    chore: update npm packages and fix test coverage.

--- gulpfile.js
+++ gulpfile.js
@@ -5,29 +5,9 @@ var plugins = require('gulp-load-plugins')();
 var through2 = require('through2');
 var os = require('os');
 var lazypipe = require('lazypipe');
-var remapIstanbul = require('remap-istanbul/lib/gulpRemapIstanbul');
 
 var tsProject = plugins.typescript.createProject('tsconfig.json', {
     typescript: require('typescript')
-});
-
-gulp.task('compile', function () {
-    var tsResult = gulp.src(['src/**/*.ts', 'custom-typings/*.d.ts'])
-        .pipe(plugins.sourcemaps.init())
-        .pipe(tsProject());
-
-    return merge([
-        tsResult.dts
-            .pipe(gulp.dest('release')),
-        tsResult.js
-            .pipe(plugins.sourcemaps.write('./', {
-                includeContent: false,
-                sourceRoot: function (file) {
-                    return path.normalize(process.cwd() + '/src');
-                }
-            }))
-            .pipe(gulp.dest('release'))
-    ]);
 });
 
 function istanbulTypeScriptIgnores() {
@@ -39,37 +19,24 @@ function istanbulTypeScriptIgnores() {
         .pipe(plugins.replace, /(var _this = _super\.call)/, ignoreReplacement)();
 }
 
-gulp.task('pre-test', ['compile'], function () {
-    return gulp.src(['release/**/*.js'])
-        .pipe(istanbulTypeScriptIgnores())
-        // Covering files
-        .pipe(plugins.istanbul())
-        // Force `require` to return covered files
-        .pipe(plugins.istanbul.hookRequire());
-});
+gulp.task('compile', function () {
+    var tsResult = gulp.src(['src/**/*.ts', 'custom-typings/*.d.ts'])
+        .pipe(plugins.sourcemaps.init())
+        .pipe(tsProject());
 
-gulp.task('test-and-coverage', ['pre-test'], function () {
-    return gulp.src('test/**/*.js', { read: false })
-        .pipe(plugins.mocha({
-            reporter: 'spec',
-            require: [
-                'babel-polyfill',
-                'babel-core/register'
-            ]
-        }))
-        .pipe(plugins.istanbul.writeReports());
-});
-
-gulp.task('run-tests', ['test-and-coverage'], function () {
-    return gulp.src('coverage/coverage-final.json')
-        .pipe(remapIstanbul({
-            reports: {
-                'json': 'coverage/remapped-coverage.json',
-                'html': 'coverage/remapped-report',
-                'lcovonly': 'coverage/remapped-lcov.info'
-            }
-        }))
-        .pipe(gulp.dest('coverage'));
+    return merge([
+        tsResult.dts
+            .pipe(gulp.dest('release')),
+        tsResult.js
+            .pipe(istanbulTypeScriptIgnores())
+            .pipe(plugins.sourcemaps.write('./', {
+                includeContent: false,
+                sourceRoot: function (file) {
+                    return path.normalize(process.cwd() + '/src');
+                }
+            }))
+            .pipe(gulp.dest('release'))
+    ]);
 });
 
 gulp.task('default', ['compile']);
